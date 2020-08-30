19d71633bd816fcf0b39b5ff0576cacf0ba40fbc
pig0726 <2065787395@qq.com>
2020-08-30 16:53:33 +0800

    update weekly contest 204

--- contest/LeetCode_Weekly_Contest_204/Maximum_Length_of_Subarray_With_Positive_Product.cpp
+++ contest/LeetCode_Weekly_Contest_204/Maximum_Length_of_Subarray_With_Positive_Product.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
-    vector<int> pcnt;
-    
-    int get(int l, int r) {
-        if (l == 0) return pcnt[r];
-        else return pcnt[r] - pcnt[l-1];
-    }
-    
     int getMaxLen(vector<int>& nums) {
         int n = nums.size();
-        pcnt.assign(n,0);
-        
-        if (nums[0] < 0) pcnt[0] = 1;
-        
-        vector<int> zero;
-        zero.push_back(-1);
-        if (nums[0] == 0) zero.push_back(0);
-        
-        for (int i = 1; i < n; ++i) {
-            int val = nums[i] < 0 ? 1 : 0;
-            pcnt[i] = pcnt[i-1] + val;
-            
-            if (nums[i] == 0) zero.push_back(i);
-        }
-        
-        zero.push_back(n);
-        
-        int ret = 0;
-        int zcnt = zero.size();
-        for (int i = 0; i < zcnt-1; ++i) {
-            int l = zero[i] + 1;
-            int r = zero[i + 1] - 1;
-            if (l > r) continue;
-            
-            int cnt = get(l, r);
-            if (cnt % 2 == 0) {
-                ret = max(ret, r - l + 1);
+        int L = 0, R = 0, ret = 0;
+        while (L < n) {
+            while (R < n && nums[R] == 0) R++;
+            L = R;
+            int neg = 0;
+            while (R < n && nums[R] != 0) {
+                if (nums[R] < 0) neg++;
+                if (!(neg & 1)) ret = max(ret, R - L + 1); 
+                R++;
             }
-            else {
-                for (int k = l; k < r; ++k) if (nums[k] < 0 && k != r) {
-                    ret = max(ret, r - k);
-                    break;
-                }
-                for (int k = r; k >= l; --k) if (nums[k] < 0 && k != l) {
-                    ret = max(ret, k-1 - l + 1);
-                    break;
-                }
+            while (L < R) {
+                if (!(neg & 1)) ret = max(ret, R - L);
+                if (nums[L] < 0) neg--;
+                L++;
             }
         }
         return ret;
