363475942f7c4839d6a18b4b697c1212400335ab
scau-flyingbird <124971687@qq.com>
2020-09-24 10:05:47 +0800

    MinCostClimbingStairs.go

--- activity/20200904/scau-flyingbird/MinCostClimbingStairs.go
+++ activity/20200904/scau-flyingbird/MinCostClimbingStairs.go
@@ -5,13 +5,13 @@ func minCostClimbingStairs(cost []int) int {
 	} else if l == 1 {
 		return cost[0]
 	}
-	dp := make([]int, l)
-	dp[0] = cost[0]
-	dp[1] = Min(cost[0], cost[1])
+	step1, step2 := cost[0], cost[1]
 	for i := 2; i < l; i++ {
-		dp[i] = Min(dp[i-1], dp[i-2]+cost[i])
+		next := Min(step1, step2) + cost[i]
+		step1 = step2
+		step2 = next
 	}
-	return dp[l-1]
+	return Min(step1, step2)
 }
 
 func Min(a, b int) int {
