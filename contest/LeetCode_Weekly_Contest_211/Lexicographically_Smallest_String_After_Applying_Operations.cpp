d5915c8307b8860a41b9313406eb76395c26233a
pig0726 <2065787395@qq.com>
2020-10-19 11:36:18 +0800

    update weekly contest 211

--- contest/LeetCode_Weekly_Contest_211/Lexicographically_Smallest_String_After_Applying_Operations.cpp
+++ contest/LeetCode_Weekly_Contest_211/Lexicographically_Smallest_String_After_Applying_Operations.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    unordered_set<string> hs;
-    int limit = 100000;
-    string ret;
-    
-    void dfs(const string& s, int a, int b, int cur) {
-        if (hs.count(s) || cur >= limit) return;
-        
-        hs.insert(s);
-        
-        ret = min(ret, s);
-        
-        string ss = s;
-        for (int i = 1; i < ss.length(); i += 2) {
-            ss[i] = ((ss[i] - '0' + a) % 10) + '0';
-        }
-        
-        dfs(ss, a, b, cur+1);
-
-        ss = s.substr(s.length()-b, b) + s.substr(0, s.length() - b);
-        dfs(ss, a, b, cur+1);
-    }
-    
     string findLexSmallestString(string s, int a, int b) {
-        ret = s;
-        dfs(s, a, b, 0);
+        int n = s.length();
+        int st0 = 0, ed0 = 10;
+        int st1 = 0, ed1 = 1;
+        if (b & 1) ed1 = 10;
+
+        string ret = s;
+        for (int a0 = st0; a0 < ed0; a0++) {
+            for (int a1 = st1; a1 < ed1; a1++) {
+                string tmp = s;
+                for (int i = 1; i < n; i += 2) {
+                    tmp[i] = ((tmp[i]-'0' + a0 * a) % 10) + '0';
+                }
+                for (int i = 0; i < n; i += 2) {
+                    tmp[i] = ((tmp[i]-'0' + a1 * a) % 10) + '0';
+                }
+            
+                for (int t = 0; t < n; t++) {
+                    int len = (t * b) % n;
+                    string tmp2 = tmp.substr(n-len, len) + tmp.substr(0, n - len);
+                    ret = min(ret, tmp2);
+                }
+            }
+        }
         return ret;
     }
 };
