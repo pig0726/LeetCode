2df201aa07c3dea4e2673bd35a54f9f0dac33690
pig0726 <2065787395@qq.com>
2020-11-02 15:47:52 +0800

    update weekly contest 213

--- contest/LeetCode_Weekly_Contest_213/Furthest_Building_You_Can_Reach.cpp
+++ contest/LeetCode_Weekly_Contest_213/Furthest_Building_You_Can_Reach.cpp
@@ -1,55 +1,26 @@
-using pii = pair<int, int>;
 class Solution {
 public:
     int furthestBuilding(vector<int>& h, int b, int ln) {
-        vector<pii> jps;
-        int n = h.size();
-        
-        vector<pii> data;
-        for (int i = 0; i < n-1; i++) if (h[i] <= h[i+1]) {
-            pii e = make_pair(h[i+1] - h[i], i);
-            data.push_back(e);
-        }
-        
-        int m = data.size();
-        if (!m) return n-1;
-        
-        int ans = -1;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        priority_queue<int> pq;
         long long bigsum = 0;
         long long sum = 0;
-        
-        for (int i = 0; i < m; i++) {
-            sum += data[i].first;
-            if (pq.size() < ln) {
-                pq.push(data[i].first);
-                bigsum += data[i].first;
-            } else if (ln > 0) {
-                int val = pq.top();
-                if (data[i].first > val) {
-                    pq.pop();
-                    bigsum -= val;
-                    pq.push(data[i].first);
-                    bigsum += data[i].first;
-                }
-            }
-            
-            if (sum - bigsum <= b) {
-                ans = data[i].second;  
-            } else {
-                break;
-            }
-        }
+        int n = h.size();
+        for (int i = 1; i < n; i++) {
+            if (h[i] <= h[i-1]) continue;
+            int d = h[i] - h[i-1];
 
-        int ret = 0;
-        for (int i = 0; i < n-1; i++) {
-            if (h[i] >= h[i+1]) {
-                ret++;
-            } else {
-                if (i <= ans) ret++;
-                else break;
+            if (pq.size() < ln) {
+                bigsum += d;
+                pq.push(d);
+            } else if (ln && pq.top() < d) {
+                bigsum -= pq.top();
+                pq.pop();
+                pq.push(d);
+                bigsum += d;
             }
+            sum += d;
+            if (sum - bigsum > b) return i-1;
         }
-        return ret;
+        return n-1;
     }
 };
